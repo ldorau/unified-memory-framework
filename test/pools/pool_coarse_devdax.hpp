51b78a4f4f3a3e7353f58c488e783a601912959c
Lukasz Dorau <lukasz.dorau@intel.com>
2024-10-22 21:51:49 +0200

    Add the jemalloc_coarse_devdax and scalable_coarse_devdax tests
    Add the jemalloc_coarse_devdax and scalable_coarse_devdax tests
    that test the coarse provider with upstream devdax provider
    and two pool managers: jemalloc and scalable pool.
    
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

---
+++ test/pools/pool_coarse_devdax.hpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Intel Corporation
+// Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef UMF_TEST_POOL_COARSE_FILE_HPP
+#define UMF_TEST_POOL_COARSE_FILE_HPP 1
+
+#include "umf/providers/provider_coarse.h"
+#include "umf/providers/provider_devdax_memory.h"
+
+#include "pool.hpp"
+#include "poolFixtures.hpp"
+
+using umf_test::test;
+using namespace umf_test;
+
+#endif /* UMF_TEST_POOL_COARSE_FILE_HPP */
