ca20ebb7f5204c8607d56cf160197ea482156f51
Lukasz Dorau <lukasz.dorau@intel.com>
2024-11-04 09:30:20 +0100

    Add IPC tests (umfIpcTest) to the devdax provider
    Signed-off-by: Lukasz Dorau <lukasz.dorau@intel.com>

---
+++ test/pools/ipc_scalable_devdax.cpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2024 Intel Corporation
+// Under the Apache License v2.0 with LLVM Exceptions. See LICENSE.TXT.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "umf/pools/pool_scalable.h"
+#include "umf/providers/provider_devdax_memory.h"
+
+#include "ipcFixtures.hpp"
+
+HostMemoryAccessor hostAccessor;
+
+auto defaultDevDaxParams = umfDevDaxMemoryProviderParamsDefault(
+    getenv("UMF_TESTS_DEVDAX_PATH"),
+    atol(getenv("UMF_TESTS_DEVDAX_SIZE") ? getenv("UMF_TESTS_DEVDAX_SIZE")
+                                         : "0"));
+
+static std::vector<ipcTestParams> ipcScalablePoolTestParamsList = {
+    {umfScalablePoolOps(), nullptr, umfDevDaxMemoryProviderOps(),
+     &defaultDevDaxParams, &hostAccessor, false},
+};
+
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(umfIpcTest);
+
+INSTANTIATE_TEST_SUITE_P(DevDaxProviderProxyPoolTest, umfIpcTest,
+                         ::testing::ValuesIn(ipcScalablePoolTestParamsList));
